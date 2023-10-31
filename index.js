f756b91e2c5de23a990bff6240de6d0528da7d4d
shaik11r <nadeencool11@gmail.com>
2023-10-31 22:43:34 +0530

    React how dom works starting article and settimeout

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 const rootNode = document.getElementById("app");
+//here rootnode has a reference of app
+//what you want the dom to look like dom elements (what user experincing right now)
 const root = ReactDOM.createRoot(rootNode);
 root.render(React.createElement(App)); //create a react custom element;
 function App() {
-  console.log("called me");
-  return React.createElement("button", null, "clickme");
+  return React.createElement(
+    "article",
+    null,
+    React.createElement("h2", null, "counter"),
+    React.createElement("p", null, "You clicked 1 times"),
+    React.createElement("button", null, "click me")
+  );
 }
-console.log("this is fun",App());
-console.log(React.createElement(App));
-//we react.createElement(app) type:"function f App()"
-// react knows this is function so calls this not expression
-/**
- * $$typeof: Symbol(react.element)
-key: null
-props: {children: 'clickme'}
-ref: null
-type: "button"
-_owner: null
-_store
-: 
-{validated: false}
- */
-/**
- * react element tree simple objects used by reacts code ;
- */
+console.log(App());
+//lets get the react things what they actually look like
+let articleEle = document.getElementsByTagName("article");
+let articleele = document.getElementsByTagName("article").item(0);
+console.log(articleEle);
+console.log(articleele);
+// these elements doesnt exits for a while
+//before react does this work asynchrously
+setTimeout(() => {
+  let articleEle = document.getElementsByTagName("article");
+  let articleele = document.getElementsByTagName("article").item(0);
+  console.log(articleEle);
+  console.log(articleele);
+}, 1000);
+/** above we can see the code because now react is executed */
+// created a tree of react elements tell react where react to add to dom and react does it work
