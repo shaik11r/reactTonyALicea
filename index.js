a6668224ab0f91cd8756de85f59ffe7ac5adffaa
shaik11r <nadeencool11@gmail.com>
2023-11-02 15:12:07 +0530

    jsx and fragment and counter app

--- index.js
+++ index.js
@@ -2,38 +2,46 @@ const rootNode = document.getElementById("app");
 //here rootnode has a reference of app
 //what you want the dom to look like dom elements (what user experincing right now)
 const root = ReactDOM.createRoot(rootNode);
-let counterName="One"
+let counterName = 1;
 root.render(React.createElement(App)); //create a react custom element;
 function App() {
-  return React.createElement(
-    "section",
-    null,
-    React.createElement("h1", null, "counters"),
-    React.createElement(
-      "section",
-      null,
-     counterName==="One"?React.createElement(Counter,{name:'ONNE'}):React.createElement(Counter,{name:'TWOO'})
-    )
+  return (
+    <section>
+      <h1>Counter</h1>
+      <section>
+        <Counter name={"nadeenshaik"}></Counter>
+        <button onClick={Update}>update</button>
+      </section>
+    </section>
   );
 }
-function Counter({name}) {
- console.log(name)
-  return React.createElement(
-    "article",
-    null,
-    React.createElement("h2", null,name),
-    React.createElement("p", null, "You Clicked 1 time"),
-    React.createElement("button", null, "Click Me")
+function Counter({ name }) {
+  console.log(name);
+  return (
+    <article>
+      <h1>Counter</h1>
+      <h2>{name}</h2>
+      <h3>here count is : {counterName}</h3>
+    </article>
   );
 }
-function Update(){
-  counterName="Two"
-  console.log("updating.....")
-  root.render(React.createElement(App))///we are saying react to render brooo
-  //nothing happens hmmm 
+function Update() {
+  counterName = counterName + 1;
+  console.log("updating.....");
+  root.render(React.createElement(App)); ///we are saying react to render brooo
+  //nothing happens hmmm
 }
 console.log(App());
 //Transpilation changing the text of code written in one syntax and converting it into
 //a different syntax that does the same thing.
 //sometimes a transpiler is reffered to as a transformer.
 //javascript syntax extension
+//jsx has no semantics im just create a new tag\
+/**
+ * funtion App(){
+ * return;
+ * }
+ */
+//fragments <> this is how we can do it because if a div is used then it will create a lot of dom
+//</> div has no sematics use div as last possible elements
+//depth of the dom tree  
