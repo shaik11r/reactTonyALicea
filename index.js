4965eadb97b92befe6119d4e9b3c9b58081e8d9f
shaik11r <nadeencool11@gmail.com>
2023-11-02 14:18:27 +0530

    rerendering

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const rootNode = document.getElementById("app");
 //here rootnode has a reference of app
 //what you want the dom to look like dom elements (what user experincing right now)
 const root = ReactDOM.createRoot(rootNode);
+let counterName="One"
 root.render(React.createElement(App)); //create a react custom element;
 function App() {
   return React.createElement(
@@ -11,21 +12,28 @@ function App() {
     React.createElement(
       "section",
       null,
-      React.createElement(Counter, { name: "nadeenshaik" }),
-      React.createElement(Counter, null)
+     counterName==="One"?React.createElement(Counter,{name:'ONNE'}):React.createElement(Counter,{name:'TWOO'})
     )
   );
 }
 function Counter({name}) {
-  name="newName"; //we can change the props but dont do it props should be immutable
-  console.log(name);
+ console.log(name)
   return React.createElement(
     "article",
     null,
-    React.createElement("h2", null, "counter"),
+    React.createElement("h2", null,name),
     React.createElement("p", null, "You Clicked 1 time"),
     React.createElement("button", null, "Click Me")
   );
 }
-
+function Update(){
+  counterName="Two"
+  console.log("updating.....")
+  root.render(React.createElement(App))///we are saying react to render brooo
+  //nothing happens hmmm 
+}
 console.log(App());
+//Transpilation changing the text of code written in one syntax and converting it into
+//a different syntax that does the same thing.
+//sometimes a transpiler is reffered to as a transformer.
+//javascript syntax extension
